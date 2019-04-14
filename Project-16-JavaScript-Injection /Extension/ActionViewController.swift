43467164a9b8ce919190f281e5850edb88080aca
GJteVelde <gerjan.te.velde@gmail.com>
2019-04-14 14:34:25 +0200

    Add Project 16: JavaScript Injection.

---
+++ Project-16-JavaScript-Injection /Extension/ActionViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Gerjan te Velde on 12/02/2019.
+//  Copyright © 2019 Gerjan te Velde. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet var script: UITextView!
+    
+    var pageTitle = ""
+    var pageURL = ""
+    var url: URL? {
+        if let url = URL(string: pageURL) {
+            return url
+        } else {
+            return nil
+        }
+    }
+
+    let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [unowned self] (dict, error) in
+                    let itemDictionary = dict as! NSDictionary
+                    let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
+                    self.pageTitle = javaScriptValues["title"] as! String
+                    self.pageURL = javaScriptValues["URL"] as! String
+                    
+                    if let host = self.url?.host, let loadedScript = self.defaults.string(forKey: host) {
+                        self.script.text = loadedScript
+                    }
+                    
+                    DispatchQueue.main.async {
+                        self.title = self.pageTitle
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func done() {
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        
+        if let host = url?.host {
+            defaults.set(script.text, forKey: host)
+        }
+        
+        self.extensionContext!.completeRequest(returningItems: [item])
+    }
+
+    @objc func adjustForKeyboard(notification: Notification) {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = UIEdgeInsets.zero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        script.scrollIndicatorInsets = script.contentInset
+        
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
+    }
+    
+}
---
+++ Project-16-JavaScript-Injection /Project 16 JavaScript Injection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 16 JavaScript Injection
+//
+//  Created by Gerjan te Velde on 12/02/2019.
+//  Copyright © 2019 Gerjan te Velde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
